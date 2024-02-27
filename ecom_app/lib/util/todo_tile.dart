03ff2b722f11f78fea4901be562601683ed20887
vidarshanaa15 <159716797+vidarshanaa15@users.noreply.github.com>
2024-02-27 19:48:04 +0530

    idkwhyimcommittingnowlol

--- ecom_app/lib/util/todo_tile.dart
+++ ecom_app/lib/util/todo_tile.dart
@@ -1,11 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:http/http.dart' as http;
 
 class ToDoTile extends StatelessWidget {
   final String taskName;
   final bool taskCompleted;
-  Function(bool?)? onChanged;
-  Function(BuildContext)? deleteFunction;
+  final Function(bool?)? onChanged;
+  final Function(BuildContext)? deleteFunction;
 
   ToDoTile({
     super.key,
@@ -34,31 +37,71 @@ class ToDoTile extends StatelessWidget {
         child: Container(
           padding: EdgeInsets.all(24),
           decoration: BoxDecoration(
-            color: Color.fromARGB(255, 183, 223, 255),
+            color: Color.fromARGB(255, 212, 238, 254),
             borderRadius: BorderRadius.circular(12),
           ),
-          child: Row(
+          child: Column(
             children: [
-              //checkbox
-              Checkbox(
-                value: taskCompleted,
-                onChanged: onChanged,
-                activeColor: Colors.black,
+              Row(
+                children: [
+                  //checkbox
+                  Checkbox(
+                    value: taskCompleted,
+                    onChanged: onChanged,
+                    activeColor: Colors.black,
+                  ),
+                  Text(
+                    taskName,
+                    style: TextStyle(
+                      decoration: taskCompleted
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                    ),
+                  ),
+                ],
+              ),
+              FutureBuilder<String>(
+                future: _fetchRandomQuote(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    // Display a loading indicator while fetching the quote
+                    return CircularProgressIndicator();
+                  } else {
+                    if (snapshot.hasError) {
+                      // Display an error message if the quote couldn't be fetched
+                      return Text('Failed to fetch quote');
+                    } else {
+                      // Display the fetched quote
+                      return Text(
+                        snapshot.data ?? '',
+                        semanticsLabel: taskName,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      );
+                    }
+                  }
+                },
               ),
 
               //task name
-              Text(
-                taskName,
-                style: TextStyle(
-                  decoration: taskCompleted
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none,
-                ),
-              ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  Future<String> _fetchRandomQuote() async {
+    final response =
+        await http.get(Uri.parse('https://api.quotable.io/random'));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data['content'];
+    } else {
+      throw Exception('Failed to fetch quote');
+    }
   }
 }
